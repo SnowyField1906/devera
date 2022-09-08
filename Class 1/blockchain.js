8c81c05e74917623fefcacfb0d3de3a25034dcbe
SnowyField <aswdqe1x@gmail.com>
2022-09-09 00:33:27 +0700

    thuan

--- Class 1/blockchain.js
+++ Class 1/blockchain.js
@@ -1,37 +1,35 @@
-import Block from './block';
+const Block = require("./block");
 
 class Blockchain {
-    constructor() {
-        this.blockchain = [this.createGenesisBlock()];
-        this.difficulty = 4;
-    }
-    createGenesisBlock() {
-        return new Block(0, "2022-09-02", "first block on the chain", "0");
-    }
-    getTheLatestBlock() {
-        return this.blockchain[this.blockchain.length - 1];
-    }
-    addNewBlock(newBlock) {
-        newBlock.previousHash = this.getTheLatestBlock().hash;
-        newBlock.hash = newBlock.generateHash();
-        this.blockchain.push(newBlock);
-        newBlock.proofOfWork(this.difficulty);
-        console.log(newBlock)
-    }
+	constructor() {
+		this.blockchain = [this.createGenesisBlock()];
+		this.difficulty = 4;
+	}
+	createGenesisBlock() {
+		return new Block(0, "2022-09-02", "first block on the chain", "0");
+	}
+	getTheLatestBlock() {
+		return this.blockchain[this.blockchain.length - 1];
+	}
+	addNewBlock(newBlock) {
+		newBlock.previousHash = this.getTheLatestBlock().hash;
+		newBlock.proofOfWork(this.difficulty);
+		this.blockchain.push(newBlock);
+	}
 
-    validateValidity() {
-        for (let i = 1; i < this.blockchain.length; i++) {
-          const currentBlock = this.blockchain[i];
-          const previousBlock = this.blockchain[i - 1];
-          if (currentBlock.hash !== currentBlock.generateHash()) {
-            return false;
-          }
-          if (currentBlock.previousHash !== previousBlock.hash) {
-            return false;
-          }
-          return true;
-        }
-      }
+	validateValidity() {
+		for (let i = 1; i < this.blockchain.length; i++) {
+			const currentBlock = this.blockchain[i];
+			const previousBlock = this.blockchain[i - 1];
+			if (currentBlock.hash !== currentBlock.generateHash()) {
+				return false;
+			}
+			if (currentBlock.previousHash !== previousBlock.hash) {
+				return false;
+			}
+			return true;
+		}
+	}
 }
 
-export default Blockchain;
+module.exports = Blockchain;
--- Class 1/index.js
+++ Class 1/index.js
@@ -4,7 +4,6 @@ const Block = require('./block');
 const deveraChain = new Blockchain();
 console.log("mining in progress...");
 
- // Add block 1
 const block1 = new Block(1, "2022-09-03", {
     sender: "Super Student",
     recipient: "Devera",
@@ -12,7 +11,6 @@ const block1 = new Block(1, "2022-09-03", {
 });
 deveraChain.addNewBlock(block1);
 
- // Add block 2
 const block2 = new Block(2, "2022-09-04", {
     sender: "Super Teacher",
     recipient: "Devera",
@@ -20,8 +18,6 @@ const block2 = new Block(2, "2022-09-04", {
 });
 deveraChain.addNewBlock(block2);
 
-// Print out the blockchain
 console.log(JSON.stringify(deveraChain, null, 5))
 
-// Check our chain for validity
 console.log("validate Validity:", deveraChain.validateValidity()) 
